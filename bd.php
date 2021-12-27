3acfdfffd864be4261a6fb75b53f2bc6dccf8db0
Jenny Ha <jhuancaan@unsa.edu.pe>
2021-12-27 10:15:03 -0500

    Change DB

--- bd.php
+++ bd.php
@@ -1,3 +1,5 @@
 <?php
-$conexion = mysqli_connect("localhost", "root", "1234", "compras");
+// $conexion = mysqli_connect("localhost", "root", "1234", "compras");
+$conexion = mysqli_connect("us-cdbr-east-05.cleardb.net", "bd97e789a86a2e", "7fda18de", "heroku_fa01200bfaa5ab0");
+
 ?>
